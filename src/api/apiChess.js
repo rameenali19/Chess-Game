f6289634aafe26dfe1e41837462eff734546b1b9
rameenali19 <rameenali1910@gmail.com>
2026-07-23 15:59:34 +0500

    Structure for frontend and backend connection

---
+++ src/api/apiChess.js
@@ -0,0 +1,32 @@
+class apiChess {
+
+  constructor() {
+    this.url = "http://localhost:3000";
+  }
+
+  //creating a new game
+  async createGame() {
+    const data = await fetch(`${url}` / games, (POST))
+  }
+
+  //get moves by ID
+  async getMoves() {
+
+  }
+
+  //get all games
+  async getAllGames() {
+
+  }
+
+  //get game by id
+  async getGame() {
+
+  }
+
+  //update game by id
+  async updateGame() {
+
+  }
+
+}
