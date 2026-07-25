16959b2d87c7f94ed6f6f4cf99381e9236659656
rameenali19 <rameenali1910@gmail.com>
2026-07-25 19:11:31 +0500

    Design changes (Added buttons)

--- src/Components/HistoryButton.jsx
+++ src/Components/HistoryButton.jsx
@@ -5,11 +5,41 @@ function HistoryButton({ game }) {
   return (
     <div className=" bg-[#FFF7EA] shadow-sm border border-[#E8DCC7]
         px-6 hover:shadow-md hover:-translate-y-0.5 transition h-20 w-full
-         max-w-4xl flex items-center text-xl rounded-lg" >
+         max-w-4xl flex items-center text-xl rounded-lg justify-between" >
 
       <span className="hover:cursor-pointer mx-7 font-inter text-xl
        text-[#17384A] font-semibold">Game # {game.id}</span>
 
+      <div className="flex gap-5 font-inter text-sm">
+
+        <button className={`w-36 px-4 py-3 rounded-full  hover:cursor-pointer
+      ${game.game_state === "finished" ? "bg-[#F7D98D] text-[#8A5A00]"
+            : "bg-[#D9E8C8] text-[#3F6B2A]"
+          }`
+        }
+        >
+          {
+            game.game_state === "finished" ? "In Progress"
+              : "Completed"
+
+          }
+        </button>
+        <button className={`w-36 px-4 py-3 rounded-xl  hover:cursor-pointer
+      ${game.game_state === "unfinished" ? "bg-[#F7D98D] text-[#8A5A00]"
+            : "ring-[#2b6381] ring-2 text-[#2b6381]"
+          }`
+        } onClick={() => {
+          navigate(`/game/${game.id}`)
+        }}>
+          {
+            game.game_state === "finished" ? "View" : "Continue"
+          }
+        </button>
+        <button className="px-6 py-3 rounded-full  hover:cursor-pointer bg-[#f4695d] text-white">
+          Delete
+        </button>
+
+      </div>
     </div>
   )
 }
