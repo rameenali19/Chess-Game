467efa8efef122705e456d31bf974602f5bb97d0
rameenali19 <rameenali1910@gmail.com>
2026-07-26 15:32:39 +0500

    Added game info

--- src/Components/GamePageInfo.jsx
+++ src/Components/GamePageInfo.jsx
@@ -3,14 +3,13 @@ function Info({ turn, checkMate, staleMate, id }) {
   const src = turn === "White" ? "/white-piece-navbar.png" : "/blue-chess-piece.png";
 
   return (
-    <div className="bg-[#F5E8D7] w-60 px-5  border-[#E8DCC7] border rounded-lg
-     mt-10 flex flex-col items-center justify-center gap-3">
+    <div className=" w-60 px-5  mt-10 flex flex-col items-center justify-center gap-3">
 
-      <div className=" bg-[#FFF7EA] shadow-sm border border-[#E8DCC7]
+      <div className=" bg-[#FFF7EA] shadow-lg border border-[#E8DCC7]
         px-2 h-21 w-55 flex flex-col py-3 rounded-lg font-inter text-[#17384A]">
         <div>
           <h1 className="font-semibold ">Current Turn</h1>
-          <div className="h-8 w-full bg-[#ff8e3d] rounded-lg px-2 py-1 flex items-center mt-1">
+          <div className="h-8 w-full bg-[#ff8e3d] rounded-lg px-2 py-1 flex items-center mt-1 ">
             <img src={src} alt="piece image " className="w-8 h-7 mr-1 object-contain" />
             {turn}
           </div>
@@ -18,25 +17,31 @@ function Info({ turn, checkMate, staleMate, id }) {
       </div>
 
 
-      <div className=" bg-[#FFF7EA]  border border-[#E8DCC7]
+      <div className=" bg-[#FFF7EA]  border border-[#E8DCC7] shadow-lg
         px-3 h-30 w-55 flex flex-col py-3 rounded-lg font-inter text-[#17384A]">
       </div>
 
 
-      <div className=" bg-[#FFF7EA] shadow-sm border border-[#E8DCC7] text-sm
-        px-3  w-55 flex flex-col py-2 rounded-lg font-inter text-[#17384A] gap-1 ">
+      <div className=" bg-[#FFF7EA] shadow-lg border border-[#E8DCC7] text-sm h-60
+        px-3  w-55 flex flex-col py-2 rounded-lg font-inter text-[#17384A] justify-between  ">
 
-        <h2 className="font-semibold text-xl text-center">
+        <div className="font-semibold text-xl justify-center flex items-center ">
+          <img src="/bulb.png" alt="blub image" className="w-8 h-8 object-contain"></img>
           Game Info
-        </h2>
+        </div>
 
         <div>
-          <p className="text-xs ">Game ID</p>
+          <div className="text-sm flex items-center justify-start ">
+            <img src="/game-id.png" alt="id image" className="w-6 h-6"></img>
+            Game ID</div>
           <p className="text-lg font-semibold">{id}</p>
         </div>
 
         <div>
-          <p className="text-xs mb-1">Game Status</p>
+          <div className="text-sm mb-1 flex items-center justify-start">
+            <img src="/game-status.png" alt="id image" className="w-5 h-6"></img>
+            Game Status
+          </div>
           <span className={` h-7 w-40 rounded-lg font-semibold py-1 px-2
           ${checkMate || staleMate ? "bg-[#D9E8C8] text-[#3F6B2A]"
               : "bg-[#F7D98D] text-[#8A5A00]"
@@ -44,8 +49,10 @@ function Info({ turn, checkMate, staleMate, id }) {
         </div>
 
         <div>
-          <p className="text-xs ">Date</p>
-          <h1 className="font-semibold">{new Date().toLocaleDateString()}</h1>
+          <div className="text-sm flex items-center justify-start ">
+            <img src="/calendar.png" alt="id image" className="w-6 h-6"></img>
+            Date</div>
+          <p className="font-semibold">{new Date().toLocaleDateString()}</p>
         </div>
 
       </div>
@@ -55,5 +62,3 @@ function Info({ turn, checkMate, staleMate, id }) {
   )
 }
 export default Info
-{/* <h1 className="font-semibold">Date</h1>
-          <h1>{new Date().toLocaleDateString()}</h1> */}
