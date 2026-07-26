6a59d795a2dd203b8d961a3b2f4401a2f7cd2b4e
rameenali19 <rameenali1910@gmail.com>
2026-07-26 17:13:54 +0500

    Updated promotion screen

--- src/Screens/PromotionScreen.jsx
+++ src/Screens/PromotionScreen.jsx
@@ -4,25 +4,36 @@ function Promotion({ turn, promote, open }) {
   const pieces = ["Queen", "Rook", "Bishop", "Knight"]
   if (open) {
     return (
-      <div className="fixed inset-0 bg-black/40 flex justify-center items-center">
+      <div className="fixed inset-0  flex justify-center items-center">
 
-        <div className="bg-white rounded-xl p-3">
-          <h2 className="text-xl font-bold mb-4">
-            Choose Promotion
-          </h2>
-          <div className="flex gap-3">
+        <div className="bg-[#FFF7EA] border border-[#E8DCC7] shadow-2xl rounded-xl p-3 h-60 w-120
+        shadow-[rgba(23,56,74,0.15)]">
+          <div className="text-3xl font-bold mb-7 flex flex-col items-center font-cormorant text-[#4A2F1D]">
+            <img src="/crown.png" alt="crown image" className="w-10 h-8"></img>
+            <div>
+              Choose Promotion
+            </div>
+          </div>
+          <div className="flex justify-between items-center gap-2">
 
             {
               pieces.map(piece => (
-                <img
-                  key={piece}
-                  src={pieceImages[turn][piece]}
-                  alt={piece}
-                  className="w-16 cursor-pointer hover:scale-110 duration-150"
-                  onClick={() => promote(piece)}
-                />
+                <div className="border-2 border-[#e4d6bb] w-30 h-30 flex flex-col items-center justify-center
+                hover:scale-105 duration-150 rounded-lg cursor-pointer hover:border-[#E67E00] 
+                hover:shadow-[0_0_40px_rgba(210,170,90,0.25)] font-cormorant text-[#4A2F1D] text-sm 
+                hover:bg-[#ffeace] font-semibold">
+                  <img
+                    key={piece}
+                    src={pieceImages[turn][piece]}
+                    alt={piece}
+                    className="w-12 "
+                    onClick={() => promote(piece)}
+                  />
+                  {piece}
+                </div>
               ))}
           </div>
+
         </div>
 
       </div>
