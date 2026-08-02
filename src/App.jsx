7fdc4770055195601f1a604629ba37e1995b0ebf
rameenali19 <rameenali1910@gmail.com>
2026-08-02 13:57:34 +0500

    socket.io-client testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import HistoryPage from "./Pages/HistoryPage";
 import HomePage from "./Pages/HomePage";
 import AboutMe from "./Pages/AboutMe";
 import Layout from "./Layout/Layout";
-
+import socket from "./api/socket";
 
 function App() {
   return (
