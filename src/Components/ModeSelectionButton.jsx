06e4fc0253396628a1cff8fb984562178ca80ac1
rameenali19 <rameenali1910@gmail.com>
2026-08-09 02:27:27 +0500

    removing components from the mode selection modal and adjusted button div width

--- src/Components/ModeSelectionButton.jsx
+++ src/Components/ModeSelectionButton.jsx
@@ -55,7 +55,7 @@ function ModeSelectionButton({ setMode }) {
         button.map((game) => {
           return (
             <div key={game.mode}
-              className="border-2 flex  h-27 w-90 rounded-lg items-center justify-around font-inter"
+              className="border-2 flex h-26 w-100 rounded-lg items-center justify-around font-inter hover:-translate-y-1 transition"
               style={{ borderColor: game.borderColor }}
             >
 
@@ -72,7 +72,7 @@ function ModeSelectionButton({ setMode }) {
                   {game.line2}</h1>
               </div>
 
-              <button className="hover:scale-105 transition hover:cursor-pointer w-27 h-9 rounded-lg font-medium text-white text-xs mr-1"
+              <button className="hover:scale-105 transition hover:cursor-pointer w-27 h-9 rounded-lg font-medium text-white text-xs mr-1 hover:opacity-80"
                 style={{ backgroundColor: game.buttonColor }}
                 onClick={() => {
                   setMode(game.mode)
--- src/Screens/ModeSelection.jsx
+++ src/Screens/ModeSelection.jsx
@@ -2,47 +2,49 @@ import MainMenu from "./MainMenu";
 import socket from "../Socket/socket";
 import { useEffect, useState } from "react";
 import ModeSelectionButton from "../Components/ModeSelectionButton";
-import ApiChess from "../api/apiChess";
-import { useContext } from "react";
-import { UserContext } from "../Context/UserContext";
+// import ApiChess from "../api/apiChess";
+// import { useContext } from "react";
+// import { UserContext } from "../Context/UserContext";
+import { motion } from "framer-motion";
 
 function ModeSelection({ mode, setMode }) {
-  const { guestId } = useContext(UserContext);
-  const [totalGames, setTotalGames] = useState(null)
+  // const { guestId } = useContext(UserContext);
+  // const [totalGames, setTotalGames] = useState(null)
 
-  useEffect(() => {
-    if (!guestId) return;
+  // useEffect(() => {
+  //   if (!guestId) return;
 
-    async function getAllGames() {
-      const game = ApiChess.getAPI();
-      const data = await game.getAllGames(1, 10, guestId);
-      setTotalGames(data.total)
-    }
-    getAllGames();
-  }, [])
+  //   async function getAllGames() {
+  //     const game = ApiChess.getAPI();
+  //     const data = await game.getAllGames(1, 10, guestId);
+  //     setTotalGames(data.total)
+  //   }
+  //   getAllGames();
+  // }, [])
 
   return (
-    <div className="flex  justify-center w-full h-full ml-45 gap-12 p-5">
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4, ease: "easeOut" }}
+      className="flex  justify-center w-full h-full ml-45 gap-10 p-5">
 
       <div className=" flex flex-col h-full w-115 justify-center ">
 
-        <div className="flex items-center gap-3 w-full mb-5">
+        <div className="flex items-center gap-3 w-full mb-4">
 
-          <button className="bg-[#1F455E] font-inter text-white rounded-lg w-22 h-7 text-xs">
-            How to play
+          <button className="bg-[#1F455E] font-inter text-white rounded-lg px-3 gap-2 justify-center h-8 text-xs flex items-center">
+            <img className="w-5 h-5"
+              src="/book.png" alt="book image"></img>
+            <div>How to play</div>
           </button>
           <div className="text-gray-700 font-medium">Move the piece to checkmate your opponent</div>
         </div>
 
-
-        <img className=" w-120 mb-1 p-3 border-2 border-[#C7A97A] rounded-lg"
+        <img className=" mb-1 p-3 border-2 border-[#C7A97A] rounded-lg"
           src="/chessboard.png" alt="chessboard image"
         ></img>
 
-
-        <div className=" w-full bg-[#FFF7EA] border-2 border-[#E8DCC7]  py-4 rounded-lg flex items-center justify-center font-inter text-xs">
-          control the center bla bla blas asjdneihri
-        </div>
       </div>
 
 
@@ -58,13 +60,13 @@ function ModeSelection({ mode, setMode }) {
             Choose a Game Mode</h1>
         </div>
 
-        <div className="flex flex-col gap-2 mt-3" >
+        <div className="flex flex-col gap-3 mt-3" >
           <ModeSelectionButton
             setMode={setMode}
           />
 
 
-          <div className="bg-[#FFF7EA] border-2 border-[#E8DCC7] w-90 h-42 flex flex-col gap-2 font-inter  rounded-lg px-2 py-2">
+          {/* <div className="bg-[#FFF7EA] border-2 border-[#E8DCC7] w-90 h-42 flex flex-col gap-2 font-inter  rounded-lg px-2 py-2">
 
             <div className="text-center font-bold text-[#443496]">Your Status</div>
 
@@ -84,7 +86,7 @@ function ModeSelection({ mode, setMode }) {
                   src="/status-win.png" alt="controller image"
                 ></img>
                 <h1>Wins</h1>
-              </div>
+              </div
               <div className="font-bold">2</div>
             </div>
 
@@ -98,13 +100,13 @@ function ModeSelection({ mode, setMode }) {
               <div className="font-bold">2</div>
             </div>
 
-          </div>
+          </div> */}
 
         </div>
 
       </div>
 
-    </div>
+    </motion.div>
   )
 }
 export default ModeSelection; 
