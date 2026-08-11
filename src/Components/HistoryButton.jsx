cffe1c9fa9b151d2cfc6d153745b8dc345691abc
rameenali19 <rameenali1910@gmail.com>
2026-08-12 01:00:01 +0500

    changed button design and added images

--- src/Components/HistoryButton.jsx
+++ src/Components/HistoryButton.jsx
@@ -18,10 +18,19 @@ function HistoryButton({ game, setDeleteModal, setSelectedGameId, guestId }) {
   const text = game.mode === "single player" ? "Single Player Game" : "Multiplayer Game"
   const textColor = game.mode === "single player" ? "text-[#eb1603]" : "text-[#ff8127]"
 
+  const statusColor = game.game_status === "finished" ? (game.winner === player?.player_color ? "bg-[#D9E8C8] text-[#3F6B2A]" : "bg-[#FCDFDD] text-[#E64743]") : "bg-[#F9E4A4] text-[#8A5A00]"
+
+  const statusText = game.game_status === "finished" ? (game.winner === player?.player_color ? "Won" : "Lost")
+    : "In Progress"
+
+  const statusImage = game.game_status === "finished" ? (game.winner === player?.player_color ? "/green-trophy.png" : "/red-cross.png") : "/pending.png"
+
   const [hover, setHover] = useState(true)
+
+
   return (
     <div className=" bg-[#FFF7EA] shadow-sm border border-[#E8DCC7]
-        px-6 hover:shadow-md hover:-translate-y-0.5 transition h-23 w-full
+        px-6 hover:shadow-md hover:-translate-y-0.5 transition h-25 w-full
           flex items-center text-xl rounded-lg justify-between" >
 
       <div className="flex items-center gap-2 ">
@@ -32,34 +41,29 @@ function HistoryButton({ game, setDeleteModal, setSelectedGameId, guestId }) {
             }`}
         ></img>
 
-        <div className="font-inter text-[#17384A]">
+        <div className="font-inter text-[#17384A] text-xs ">
           <h1 className="text-xl font-semibold">{text}</h1>
-          <div className="flex gap-1">
-            <div>
-              <h1 className="text-xs ">You : </h1>
-              <h1 className="text-xs ">Opponent : </h1>
-            </div>
-            <div className={`text-xs font-semibold ${textColor}`}>
-              <h1>{player?.player_color}</h1>
-              <h1> {player?.player_color === "White" ? "Black" : "White"}</h1>
-            </div>
-          </div>
+          <h1>Player color :
+            <span className={` font-semibold ${textColor}`}>{player?.player_color} </span>
+          </h1>
         </div>
       </div>
 
-      <div className="flex gap-5 font-inter text-sm font-semibold">
 
-        <button className={`w-36 px-4  rounded-2xl  hover:cursor-pointer hover:-translate-y-0.5 transition
-      ${game.game_status === "unfinished" || game.game_status === "waiting" ? "bg-[#F9E4A4] text-[#8A5A00]"
-            : "bg-[#D9E8C8] text-[#3F6B2A]"
-          }`}
-        >
-          {
-            game.game_status === "unfinished" || game.game_status === "waiting" ? "In Progress" : "Completed"
+      <div className="flex gap-5 font-inter text-sm font-semibold items-center">
 
-          }
+        <button className={` flex w-37 h-15 rounded-2xl hover:cursor-pointer  hover:-translate-y-0.5 transition 
+          ${statusColor} items-center justify-center gap-3`} >
+          <img className="object-contain w-9 h-9"
+            src={statusImage} alt="status Image">
+          </img>
+          <div>
+            <h1 className="font-bold">{statusText}</h1>
+          </div>
         </button>
-        <button className={`w-36 px-4 py-3 rounded-xl  hover:cursor-pointer hover:-translate-y-0.5 transition
+
+
+        <button className={`w-33 h-11 rounded-xl  hover:cursor-pointer hover:-translate-y-0.5 transition
       ${game.game_status === "unfinished" || game.game_status === "waiting" ? "bg-[#2b6381] text-white"
             : "ring-[#2b6381] ring-2 text-[#2b6381] "
           }`
@@ -81,7 +85,9 @@ function HistoryButton({ game, setDeleteModal, setSelectedGameId, guestId }) {
             game.game_status === "finished" ? "View" : "Continue"
           }
         </button>
-        <button className="px-4 rounded-full  hover:cursor-pointer bg-[#eb1603] text-white hover:-translate-y-0.5 transition flex items-center"
+
+
+        <button className="w-25 h-12 rounded-full  hover:cursor-pointer bg-[#E64743] text-white hover:-translate-y-0.5 transition flex justify-center items-center"
           onClick={() => {
             setDeleteModal(true)
             setSelectedGameId(game.id)
@@ -97,7 +103,7 @@ function HistoryButton({ game, setDeleteModal, setSelectedGameId, guestId }) {
         </button>
 
       </div>
-    </div>
+    </div >
   )
 }
 export default HistoryButton
