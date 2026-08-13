ece0bf14c5bb000fab66481b242245cdd1073f7d
rameenali19 <rameenali1910@gmail.com>
2026-08-13 23:53:10 +0500

    making open prop in Modal.jsx

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,22 +1,26 @@
 import { motion } from "framer-motion";
 
-function Modal({ className, children }) {
+function Modal({ open, className, children }) {
+  if (open) {
 
-  return (
-    <div className="fixed inset-0 flex justify-center items-center bg-[#3E2C20]/25 z-50">
+    return (
+      <div className="fixed inset-0 flex justify-center items-center bg-[#3E2C20]/25 z-50">
 
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4, ease: "easeOut" }}
-        className={`border border-[#E8DCC7] shadow-2xl rounded-xl
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, ease: "easeOut" }}
+          className={`border border-[#E8DCC7] shadow-2xl rounded-xl
          shadow-[rgba(23,56,74,0.15)] bg-[url('/orangebg.png')]
          bg-center bg-cover ${className}`}>
 
-        {children}
+          {children}
 
-      </motion.div>
-    </div>
-  )
+        </motion.div>
+      </div>
+    )
+
+  }
+
 }
 export default Modal;
