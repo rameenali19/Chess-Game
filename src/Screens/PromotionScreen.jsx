bdcb44e697d244e5d35ff8f5cb443431551f14ff
rameenali19 <rameenali1910@gmail.com>
2026-08-13 23:27:05 +0500

    adjusting width in promotion screen

--- src/Screens/PromotionScreen.jsx
+++ src/Screens/PromotionScreen.jsx
@@ -8,18 +8,18 @@ function Promotion({ turn, promote, open }) {
 
       <Modal className="h-60 w-120">
 
-        <div className="text-3xl font-bold mb-7 flex flex-col items-center font-cormorant text-[#4A2F1D]">
+        <div className="text-3xl font-bold p-4 flex flex-col items-center font-cormorant text-[#4A2F1D]">
           <img src="/crown.png" alt="crown image" className="w-10 h-8 hover:scale-120 duration-150"></img>
           <div>
             Choose Promotion
           </div>
         </div>
-        <div className="flex justify-between items-center gap-2">
+        <div className="flex justify-around items-center ">
 
           {
             pieces.map(piece => (
               <div key={piece}
-                className="border-2 border-[#e4d6bb] w-30 h-30 flex flex-col items-center justify-center
+                className="border-2 border-[#e4d6bb] w-25 h-25 flex flex-col items-center justify-center
                 hover:scale-105 duration-150 rounded-lg cursor-pointer hover:border-[#E67E00] 
                 hover:shadow-[0_0_40px_rgba(210,170,90,0.25)] font-cormorant text-[#4A2F1D] text-sm 
                 hover:bg-[#fff4e6] font-semibold"
