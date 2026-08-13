9dfe4a9fe22d64e3a07424b11114591476777318
rameenali19 <rameenali1910@gmail.com>
2026-08-13 12:45:49 +0500

    refactored HistoryNavBar.jsx

--- src/Components/HistoryNavBar.jsx
+++ src/Components/HistoryNavBar.jsx
@@ -1,32 +1,42 @@
+import Button from "./Button"
+
 function HistoryNavBar({ games, filter, setFilter }) {
-
+  const filters = [
+    {
+      filter: "all",
+      text: "All Games"
+    },
+    {
+      filter: "finished",
+      text: "Completed"
+    },
+    {
+      filter: "unfinished",
+      text: "In Progress"
+    }
+  ]
   return (
+    <nav className="font-inter text-lg text-[#17384A] shadow-sm border-b-[#E8DCC7] px-6
+     h-15 w-full flex items-center justify-start mb-4 gap-15">
+      {
+        filters.map((button) => {
+          return (
+            <Button
+              text={button.text}
+              variant="outline"
+              textSize="large"
+              textWeight="normal"
+              onClick={() => { setFilter(button.filter) }}
+              className={
+                filter === button.filter ?
+                  "underline decoration-2 decoration-amber-600 underline-offset-4 text-amber-600"
+                  : ""
+              }
+            />
+          )
+        })
+      }
 
-    <nav className="font-inter text-lg text-[#17384A] shadow-sm  
-        border-b-[#E8DCC7] px-6 h-15 w-full  flex 
-        items-center justify-start mb-4 gap-15">
-
-      <div onClick={() => { setFilter("all") }}
-        className={`hover:cursor-pointer ${filter === "all" ?
-          "underline decoration-2 decoration-amber-600 underline-offset-4 text-amber-600"
-          : ""
-          }`}
-      >All Games
-      </div>
-      <div onClick={() => { setFilter("finished") }}
-        className={`hover:cursor-pointer ${filter === "finished" ?
-          "underline decoration-2 decoration-amber-600 underline-offset-4 text-amber-600"
-          : ""
-          }`}
-      >Completed
-      </div>
-      <div onClick={() => { setFilter("unfinished") }}
-        className={`hover:cursor-pointer ${filter === "unfinished" ?
-          "underline decoration-2 decoration-amber-600 underline-offset-4 text-amber-600"
-          : ""
-          }`}
-      >In Progress
-      </div>
     </nav>
   )
 }
