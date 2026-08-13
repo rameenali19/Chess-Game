0044a76f78e572ab360179a2dcd3680067eef9fe
rameenali19 <rameenali1910@gmail.com>
2026-08-13 22:51:01 +0500

    changed join screen

--- src/Screens/JoinScreen.jsx
+++ src/Screens/JoinScreen.jsx
@@ -5,6 +5,7 @@ import SocketClass from "../Socket/socketClass";
 import { useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 import Button from "../Components/Button";
+import Modal from "../Components/Modal";
 
 function JoinScreen({ setGameId, setMode }) {
 
@@ -34,90 +35,86 @@ function JoinScreen({ setGameId, setMode }) {
 
 
   return (
-    <div className="fixed inset-0  flex justify-center items-center bg-[#3E2C20]/25 z-50">
 
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4, ease: "easeOut" }}
-        className="bg-[url('/orangebg.png')] bg-center bg-cover border border-[#E8DCC7] shadow-2xl
-         rounded-xl h-65 w-140 shadow-[rgba(23,56,74,0.15)] ">
+    <Modal className="h-65 w-140">
 
-        {!error && (
-          <div className="flex flex-col h-full justify-around px-4 w-full">
+      {!error && (
+        <div className="flex flex-col h-full justify-around px-4 w-full">
 
-            <div className="flex flex-col items-center gap-2 mt-6">
-              <h1 className="text-3xl font-bold font-cormorant text-[#17384A]">
-                Join Game
-              </h1>
-              <h1 className="text-xs font-inter text-[#17384A]">
-                Enter the Game ID to join your friend
-              </h1>
-            </div>
-
-            <form className="flex flex-col gap-3 items-center"
-              onSubmit={checkingId}>
-
-              <input
-                className="ring ring-[#ff8127] rounded-lg w-70 py-3 px-4 bg-[#FFF7EA]/70 
-              hover:scale-105 transition outline-none  focus:ring-2 hover:ring-2"
-                placeholder="Enter Game ID"
-                inputMode="numeric"
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-              />
-              <Button
-                text="Join Game"
-                variant="primary"
-                textSize="normal"
-                fontWeight="normal"
-                className="w-40 py-2"
-                type="submit"
-              />
-
-            </form>
-
-            <div className="flex justify-end ">
-
-              <Button
-                text="Close"
-                variant="sideBarBlue"
-                textSize="small"
-                fontWeight="medium"
-                className="w-17 h-7"
-                onClick={() => {
-                  leaveGame()
-                }}
-              />
-
-            </div>
+          <div className="flex flex-col items-center gap-2 mt-6">
+            <h1 className="text-3xl font-bold font-cormorant text-[#17384A]">
+              Join Game
+            </h1>
+            <h1 className="text-xs font-inter text-[#17384A]">
+              Enter the Game ID to join your friend
+            </h1>
           </div>
-        )}
 
+          <form className="flex flex-col gap-3 items-center"
+            onSubmit={checkingId}>
 
-        {error && (
-          <div className="flex flex-col h-full justify-center items-center gap-8">
-
-            <div className="text-3xl font-bold font-cormorant text-[#17384A]">
-              Invalid Game ID
-            </div>
-
+            <input
+              className="ring ring-[#ff8127] rounded-lg w-70 py-3 px-4 bg-[#FFF7EA]/70 
+              hover:scale-105 transition outline-none  focus:ring-2 hover:ring-2"
+              placeholder="Enter Game ID"
+              inputMode="numeric"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
             <Button
-              text="Try Again"
+              text="Join Game"
               variant="primary"
               textSize="normal"
               fontWeight="normal"
               className="w-40 py-2"
+              type="submit"
+            />
+
+          </form>
+
+          <div className="flex justify-end ">
+
+            <Button
+              text="Close"
+              variant="sideBarBlue"
+              textSize="small"
+              fontWeight="medium"
+              className="w-17 h-7"
               onClick={() => {
-                setError(false)
-                setInput("")
+                leaveGame()
               }}
             />
 
           </div>
-        )}
-      </motion.div>
-    </div >
+        </div>
+      )}
+
+
+      {error && (
+        <div className="flex flex-col h-full justify-center items-center gap-8">
+
+          <div className="text-3xl font-bold font-cormorant text-[#17384A]">
+            Invalid Game ID
+          </div>
+
+          <Button
+            text="Try Again"
+            variant="primary"
+            textSize="normal"
+            fontWeight="normal"
+            className="w-40 py-2"
+            onClick={() => {
+              setError(false)
+              setInput("")
+            }}
+          />
+
+        </div>
+      )}
+
+
+
+    </Modal>
 
   )
 
