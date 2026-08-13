155ada772b21d6742dab8ed835cc13b09ff32c01
rameenali19 <rameenali1910@gmail.com>
2026-08-13 22:23:38 +0500

    creating basic Modal.jsx structure

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,22 @@
+import { motion } from "framer-motion";
+
+function Modal({ propotion, modal }) {
+
+  return (
+    <div className="fixed inset-0  flex justify-center items-center bg-[#3E2C20]/25 z-50">
+
+      < motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, ease: "easeOut" }}
+        className={`border border-[#E8DCC7] shadow-2xl rounded-xl
+         shadow-[rgba(23,56,74,0.15)] bg-[url('/orangebg.png')]
+         bg-center bg-cover ${propotion}`}>
+
+        {modal}
+
+      </motion.div>
+    </div>
+  )
+}
+export default Modal;
