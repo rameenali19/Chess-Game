92d012878437790e83a7d1a39ddd53cdc7c01c3e
rameenali19 <rameenali1910@gmail.com>
2026-08-13 12:17:44 +0500

    shifting const inside the Button function

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,29 +1,3 @@
-const buttonVariants = {
-  primary:
-    "bg-amber-600 text-[rgb(248,240,225)]",
-  outline:
-    "text-[#17384A] hover:underline hover:decoration-2 hover:decoration-amber-600 hover:underline-offset-4 hover:text-amber-600"
-};
-
-const buttonSizes = {
-  medium: "px-5 py-2 ",
-};
-
-const textWeights = {
-  normal: "font-normal",
-  medium: "font-medium",
-  semiBold: "font-semibold"
-};
-
-const textSizes = {
-  normal: "text-base",
-  small: "text-xs",
-  large: "text-lg"
-}
-
-const baseButtonStyle =
-  "rounded-lg transition hover:cursor-pointer hover:scale-105 hover:opacity-85 font-inter";
-
 function Button({
   text = "",
   variant = "",
@@ -33,6 +7,33 @@ function Button({
   onClick = "",
   className: customClassName = ""
 }) {
+
+  const buttonVariants = {
+    primary:
+      "bg-amber-600 text-[rgb(248,240,225)]",
+    outline:
+      "text-[#17384A] hover:underline hover:decoration-2 hover:decoration-amber-600 hover:underline-offset-4 hover:text-amber-600"
+  };
+
+  const buttonSizes = {
+    medium: "px-5 py-2 ",
+  };
+
+  const textWeights = {
+    normal: "font-normal",
+    medium: "font-medium",
+    semiBold: "font-semibold"
+  };
+
+  const textSizes = {
+    normal: "text-base",
+    small: "text-xs",
+    large: "text-lg"
+  }
+
+  const baseButtonStyle =
+    "rounded-lg transition hover:cursor-pointer hover:scale-105 hover:opacity-85 font-inter";
+
 
   const className = [
     baseButtonStyle,
