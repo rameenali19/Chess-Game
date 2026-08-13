36b77aa5357afcf9b8c7c31675911f84b42b7497
rameenali19 <rameenali1910@gmail.com>
2026-08-14 00:08:54 +0500

    changing disconnect screen and sending open as prop to Modal.jsx

--- src/Screens/DisconnectScreen.jsx
+++ src/Screens/DisconnectScreen.jsx
@@ -1,62 +1,64 @@
 import { Navigate, useNavigate } from "react-router-dom"
 import Button from "../Components/Button"
+import Modal from "../Components/Modal"
 
 function DisconnectScreen({ open, setDisconnectScreen, setWaitingScreen, setReconnectingScreen }) {
   const navigate = useNavigate()
-  if (open) {
-    return (
-      <div className="fixed inset-0  flex justify-center items-center bg-[#3E2C20]/25 z-50">
 
-        <div className="bg-[url('/orangebg.png')] bg-center bg-cover border border-[#E8DCC7] shadow-2xl rounded-xl p-3 h-65 w-125 shadow-[rgba(23,56,74,0.15)] flex flex-col items-center justify-center gap-7">
+  return (
+    <Modal open={open}
+      className="h-65 w-125">
 
-          <div className=" flex flex-col items-center gap-3 text-[#17384A]">
-            <h1 className="text-3xl font-bold font-cormorant">
-              Connection Lost
-            </h1>
-            <h1 className="font-inter text-xs">
-              Your opponent is currently unavailable</h1>
-            <h1 className="font-inter text-xs"> You can wait for them to return or leave the game.</h1>
-          </div>
+      <div className="flex flex-col items-center justify-center gap-7">
 
-          <div className="flex justify-center items-center gap-4">
-
-            <Button
-              text="Wait"
-              variant="wait"
-              textSize="large"
-              fontWeight="medium"
-              className="w-35 items-center flex justify-center gap-1 py-1"
-              onClick={() => {
-                setDisconnectScreen(false)
-                setReconnectingScreen(true)
-              }}
-              image="/sand-clock.png"
-              imageText="sand clock image"
-              imageStyling="object-contain w-5 h-5"
-            />
-
-            <Button
-              text="Leave"
-              variant="leave"
-              textSize="large"
-              fontWeight="medium"
-              className="w-35 items-center flex justify-center gap-1 py-1"
-              onClick={() => {
-                setDisconnectScreen(false)
-                navigate(`/modeselection`)
-              }}
-              image="/door.png"
-              imageText="door image"
-              imageStyling="object-contain w-5 h-5"
-            />
-
-          </div>
-
+        <div className=" flex flex-col items-center gap-3 text-[#17384A]">
+          <h1 className="text-3xl font-bold font-cormorant">
+            Connection Lost
+          </h1>
+          <h1 className="font-inter text-xs">
+            Your opponent is currently unavailable</h1>
+          <h1 className="font-inter text-xs"> You can wait for them to return or leave the game.</h1>
         </div>
 
+        <div className="flex justify-center items-center gap-4">
+
+          <Button
+            text="Wait"
+            variant="wait"
+            textSize="large"
+            fontWeight="medium"
+            className="w-35 items-center flex justify-center gap-1 py-1"
+            onClick={() => {
+              setDisconnectScreen(false)
+              setReconnectingScreen(true)
+            }}
+            image="/sand-clock.png"
+            imageText="sand clock image"
+            imageStyling="object-contain w-5 h-5"
+          />
+
+          <Button
+            text="Leave"
+            variant="leave"
+            textSize="large"
+            fontWeight="medium"
+            className="w-35 items-center flex justify-center gap-1 py-1"
+            onClick={() => {
+              setDisconnectScreen(false)
+              navigate(`/modeselection`)
+            }}
+            image="/door.png"
+            imageText="door image"
+            imageStyling="object-contain w-5 h-5"
+          />
+
+        </div>
       </div>
-    )
-  }
+
+    </Modal>
+
+  )
+
 
 }
 export default DisconnectScreen
