c58433bb23a9d48257f271b11b20eee41b5c1bcb
rameenali19 <rameenali1910@gmail.com>
2026-08-13 23:10:15 +0500

    space fix

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -5,7 +5,7 @@ function Modal({ className, children }) {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-[#3E2C20]/25 z-50">
 
-      < motion.div
+      <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.4, ease: "easeOut" }}
