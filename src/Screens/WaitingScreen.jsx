1b73826e2a586a87a887ebf276387e8910a4da62
rameenali19 <rameenali1910@gmail.com>
2026-08-13 22:47:10 +0500

    changed waiting screen

--- src/Screens/WaitingScreen.jsx
+++ src/Screens/WaitingScreen.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import SocketClass from "../Socket/socketClass";
 import { motion } from "framer-motion";
 import Button from "../Components/Button";
+import Modal from "../Components/Modal";
 
 function WaitingScreen({ setWaitingScreen, gameId, setMode }) {
   const navigate = useNavigate();
@@ -17,81 +18,75 @@ function WaitingScreen({ setWaitingScreen, gameId, setMode }) {
   }
 
   return (
-    <div className="fixed inset-0  flex justify-center items-center bg-[#3E2C20]/25 z-50">
 
-      < motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.4, ease: "easeOut" }}
-        className=" border border-[#E8DCC7] shadow-2xl rounded-xl h-65 w-140
-        shadow-[rgba(23,56,74,0.15)] bg-[url('/orangebg.png')]
-         bg-center bg-cover">
+    <Modal className="h-65 w-140">
 
-        <div className=" flex flex-col justify-center items-center w-full h-full gap-3 px-3">
-          {
-            !leavingGame && (
-              <>
-                <div className="text-3xl font-bold font-cormorant text-[#17384A] mt-5">
-                  Waiting for Opponent
-                </div>
-                <div className="text-[#17384A] font-inter text-xs ">
-                  Share the game ID with your friend
-                </div>
-                <div className="text-[#17384A] font-inter text-xl font-bold  ">
-                  {gameId}
-                </div>
+      <div className=" flex flex-col justify-center items-center w-full h-full gap-3 px-3">
+        {
+          !leavingGame && (
+            <>
+              <div className="text-3xl font-bold font-cormorant text-[#17384A] mt-5">
+                Waiting for Opponent
+              </div>
+              <div className="text-[#17384A] font-inter text-xs ">
+                Share the game ID with your friend
+              </div>
+              <div className="text-[#17384A] font-inter text-xl font-bold  ">
+                {gameId}
+              </div>
 
-                <div
-                  className="w-12 h-12 rounded-full border-4 border-[#ffc79f]
+              <div
+                className="w-12 h-12 rounded-full border-4 border-[#ffc79f]
                  border-t-[#ff8127] animate-spin" />
-                <div className="flex justify-end w-full">
+              <div className="flex justify-end w-full">
 
-                  <Button
-                    text="Leave"
-                    variant="sideBarBlue"
-                    textSize="small"
-                    fontWeight="medium"
-                    className="w-20 h-8"
-                    onClick={() => {
-                      setLeavingGame(true)
-                    }} />
-                </div>
-              </>
-            )}
-          {
-            leavingGame && (
-              <>
-                <div className="text-3xl font-bold font-cormorant  text-[#17384A] ">
-                  Leave the waiting room?
-                </div>
+                <Button
+                  text="Leave"
+                  variant="sideBarBlue"
+                  textSize="small"
+                  fontWeight="medium"
+                  className="w-20 h-8"
+                  onClick={() => {
+                    setLeavingGame(true)
+                  }} />
+              </div>
+            </>
+          )}
+        {
+          leavingGame && (
+            <>
+              <div className="text-3xl font-bold font-cormorant  text-[#17384A] ">
+                Leave the waiting room?
+              </div>
 
-                <div className="flex w-full justify-center gap-10 items-center mt-5">
+              <div className="flex w-full justify-center gap-10 items-center mt-5">
 
-                  <Button
-                    text="Leave"
-                    variant="sideBarBlue"
-                    textSize="small"
-                    fontWeight="medium"
-                    className="w-20 h-8"
-                    onClick={() => {
-                      leaveGame()
-                    }} />
+                <Button
+                  text="Leave"
+                  variant="sideBarBlue"
+                  textSize="small"
+                  fontWeight="medium"
+                  className="w-20 h-8"
+                  onClick={() => {
+                    leaveGame()
+                  }} />
 
-                  <Button
-                    text="Stay"
-                    variant="sideBarBlue"
-                    textSize="small"
-                    fontWeight="medium"
-                    className="w-20 h-8"
-                    onClick={() => {
-                      setLeavingGame(false)
-                    }} />
-                </div>
-              </>
-            )}
-        </div>
-      </motion.div>
-    </div >
+                <Button
+                  text="Stay"
+                  variant="sideBarBlue"
+                  textSize="small"
+                  fontWeight="medium"
+                  className="w-20 h-8"
+                  onClick={() => {
+                    setLeavingGame(false)
+                  }} />
+              </div>
+            </>
+          )}
+      </div>
+
+    </Modal>
+
   )
 }
 export default WaitingScreen
