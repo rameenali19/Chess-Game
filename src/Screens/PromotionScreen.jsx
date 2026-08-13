f387b38da7209b081c9b348f2db3fea4670473dd
rameenali19 <rameenali1910@gmail.com>
2026-08-14 00:02:11 +0500

    sending open prop from promotion screen modal

--- src/Screens/PromotionScreen.jsx
+++ src/Screens/PromotionScreen.jsx
@@ -3,41 +3,42 @@ import Modal from "../Components/Modal";
 function Promotion({ turn, promote, open }) {
 
   const pieces = ["Queen", "Rook", "Bishop", "Knight"]
-  if (open) {
-    return (
 
-      <Modal className="h-60 w-120">
+  return (
 
-        <div className="text-3xl font-bold p-4 flex flex-col items-center font-cormorant text-[#4A2F1D]">
-          <img src="/crown.png" alt="crown image" className="w-10 h-8 hover:scale-120 duration-150"></img>
-          <div>
-            Choose Promotion
-          </div>
+    <Modal open={open}
+      className="h-60 w-120">
+
+      <div className="text-3xl font-bold p-4 flex flex-col items-center font-cormorant text-[#4A2F1D]">
+        <img src="/crown.png" alt="crown image" className="w-10 h-8 hover:scale-120 duration-150"></img>
+        <div>
+          Choose Promotion
         </div>
-        <div className="flex justify-around items-center ">
+      </div>
+      <div className="flex justify-around items-center ">
 
-          {
-            pieces.map(piece => (
-              <div key={piece}
-                className="border-2 border-[#e4d6bb] w-25 h-25 flex flex-col items-center justify-center
+        {
+          pieces.map(piece => (
+            <div key={piece}
+              className="border-2 border-[#e4d6bb] w-25 h-25 flex flex-col items-center justify-center
                 hover:scale-105 duration-150 rounded-lg cursor-pointer hover:border-[#E67E00] 
                 hover:shadow-[0_0_40px_rgba(210,170,90,0.25)] font-cormorant text-[#4A2F1D] text-sm 
                 hover:bg-[#fff4e6] font-semibold"
-                onClick={() => promote(piece)}
-              >
-                <img
+              onClick={() => promote(piece)}
+            >
+              <img
 
-                  src={pieceImages[turn][piece]}
-                  alt={piece}
-                  className="w-12 "
-                />
-                {piece}
-              </div>
-            ))}
-        </div>
-      </Modal>
-    )
-  }
+                src={pieceImages[turn][piece]}
+                alt={piece}
+                className="w-12 "
+              />
+              {piece}
+            </div>
+          ))}
+      </div>
+    </Modal>
+  )
+
 
 }
 export default Promotion;
