b6c70ba24fdc7a9dcbe307f2c8d0837030f4b640
rameenali19 <rameenali1910@gmail.com>
2026-08-15 04:22:37 +0500

    making resign and restart game buttons

--- src/Components/RightPanelButtons.jsx
+++ src/Components/RightPanelButtons.jsx
@@ -1,8 +1,32 @@
+import Button from "../Components/Button"
+
 function RightPanelButtons() {
 
   return (
     <div className=" bg-[#FFF7EA] shadow-lg border border-[#E8DCC7] text-sm h-30
-      w-70 flex flex-col  rounded-lg font-inter text-[#17384A] justify-around px-2 ">
+      w-70 flex flex-col  rounded-lg font-inter text-[#17384A] justify-center gap-3 items-center  ">
+
+      <Button
+        text="Resign"
+        variant="red"
+        textSize="large"
+        fontWeight="semiBold"
+        className="w-60 items-center flex justify-center h-10 gap-2"
+        image="/white-flag.png"
+        imageText="white flag image"
+        imageStyling="object-contain w-5 h-5"
+      />
+
+      <Button
+        text="Restart Game"
+        variant="viewButton"
+        textSize="large"
+        fontWeight="semiBold"
+        className=" w-60 items-center flex justify-center h-10 gap-1"
+        image="/blue-reload.png"
+        imageText="blue reload image"
+        imageStyling="object-contain w-5 h-5"
+      />
 
     </div>
 
