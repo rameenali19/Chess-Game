f9dae44d9b7253995353c2f72d2d24d12b773311
rameenali19 <rameenali1910@gmail.com>
2026-08-15 01:25:33 +0500

    using CurrentTurnDisplay.jsx inside GameInfoPage.jsx

--- src/Components/CurrentTurnDisplay.jsx
+++ src/Components/CurrentTurnDisplay.jsx
@@ -1,4 +1,17 @@
-function CurrentTurnDisplay() {
+function CurrentTurnDisplay({ src, turn }) {
 
+  return (
+    <div className=" bg-[#FFF7EA] shadow-lg border border-[#E8DCC7]
+        px-2 h-21 w-55 flex flex-col justify-center rounded-lg font-inter text-[#17384A] gap-2">
+
+      <h1 className="font-semibold ">Current Turn</h1>
+      <div className="h-8 w-full bg-[#ff8e3d] rounded-lg px-2  flex items-center
+           hover:scale-105 duration-150 ">
+        <img src={src} alt="piece image " className="w-8 h-7 mr-1 object-contain" />
+        {turn}
+      </div>
+
+    </div>
+  )
 }
 export default CurrentTurnDisplay;
--- src/Components/GamePageInfo.jsx
+++ src/Components/GamePageInfo.jsx
@@ -15,18 +15,10 @@ function Info({ turn, checkMate, staleMate, id, userColor, opponentColor }) {
         text="Opponent"
       />
 
-      <div className=" bg-[#FFF7EA] shadow-lg border border-[#E8DCC7]
-        px-2 h-21 w-55 flex flex-col justify-center rounded-lg font-inter text-[#17384A] gap-2">
-
-        <h1 className="font-semibold ">Current Turn</h1>
-        <div className="h-8 w-full bg-[#ff8e3d] rounded-lg px-2  flex items-center
-           hover:scale-105 duration-150 ">
-          <img src={src} alt="piece image " className="w-8 h-7 mr-1 object-contain" />
-          {turn}
-        </div>
-
-      </div>
-
+      <CurrentTurnDisplay
+        src={src}
+        turn={turn}
+      />
 
       <div className=" bg-[#FFF7EA] shadow-lg border border-[#E8DCC7] text-sm h-60
           w-55 flex flex-col  rounded-lg font-inter text-[#17384A] justify-around px-2 ">
