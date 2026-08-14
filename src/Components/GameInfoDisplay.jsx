9baf8021e2403300ae93ad6c979c18f001d5244b
rameenali19 <rameenali1910@gmail.com>
2026-08-15 01:50:32 +0500

    using GameInfoDisplay.jsx inside gamePageInfo.jsx

--- src/Components/GameInfoDisplay.jsx
+++ src/Components/GameInfoDisplay.jsx
@@ -1,5 +1,39 @@
-function GameInfoDisplay() {
+function GameInfoDisplay({ color, text, id }) {
 
+  return (
+    <div className=" bg-[#FFF7EA] shadow-lg border border-[#E8DCC7] text-sm h-60
+          w-55 flex flex-col  rounded-lg font-inter text-[#17384A] justify-around px-2 ">
 
+      <div className="font-semibold text-xl justify-center flex items-center ">
+        <img src="/bulb.png" alt="blub image" className="w-8 h-8 object-contain"></img>
+        Game Info
+      </div>
+
+      <div>
+        <div className="text-sm flex items-center justify-start ">
+          <img src="/game-id.png" alt="id image" className="w-6 h-6"></img>
+          Game ID</div>
+        <p className="text-lg font-semibold">{id}</p>
+      </div>
+
+      <div>
+        <div className="text-sm mb-1 flex items-center justify-start">
+          <img src="/game-status.png" alt="id image" className="w-5 h-6"></img>
+          Game Status
+        </div>
+        <div className={` h-7 w-40 rounded-lg font-semibold 
+          py-1 px-2 hover:scale-105 duration-150
+          ${color}`}>{text}
+        </div>
+      </div>
+
+      <div>
+        <div className="text-sm flex items-center justify-start ">
+          <img src="/calendar.png" alt="id image" className="w-6 h-6"></img>
+          Date</div>
+        <p className="font-semibold">{new Date().toLocaleDateString()}</p>
+      </div>
+    </div>
+  )
 }
 export default GameInfoDisplay;
--- src/Components/GamePageInfo.jsx
+++ src/Components/GamePageInfo.jsx
@@ -12,13 +12,11 @@ function Info({ turn, checkMate, staleMate, id, userColor, opponentColor }) {
       color: "bg-[#F7D98D] text-[#8A5A00]",
       text: "In Progress"
     },
-
     complete: {
       color: "bg-[#D9E8C8] text-[#3F6B2A]",
       text: "Completed"
     }
   }
-
   const gameStatusDiv = statusColor[checkMate || staleMate ? "complete" : "onGoing"]
 
   return (
@@ -34,39 +32,11 @@ function Info({ turn, checkMate, staleMate, id, userColor, opponentColor }) {
         turn={turn}
       />
 
-      <div className=" bg-[#FFF7EA] shadow-lg border border-[#E8DCC7] text-sm h-60
-          w-55 flex flex-col  rounded-lg font-inter text-[#17384A] justify-around px-2 ">
-
-        <div className="font-semibold text-xl justify-center flex items-center ">
-          <img src="/bulb.png" alt="blub image" className="w-8 h-8 object-contain"></img>
-          Game Info
-        </div>
-
-        <div>
-          <div className="text-sm flex items-center justify-start ">
-            <img src="/game-id.png" alt="id image" className="w-6 h-6"></img>
-            Game ID</div>
-          <p className="text-lg font-semibold">{id}</p>
-        </div>
-
-        <div>
-          <div className="text-sm mb-1 flex items-center justify-start">
-            <img src="/game-status.png" alt="id image" className="w-5 h-6"></img>
-            Game Status
-          </div>
-          <div className={` h-7 w-40 rounded-lg font-semibold py-1 px-2 hover:scale-105 duration-150
-          ${gameStatusDiv.color}`}>{gameStatusDiv.text}
-          </div>
-        </div>
-
-        <div>
-          <div className="text-sm flex items-center justify-start ">
-            <img src="/calendar.png" alt="id image" className="w-6 h-6"></img>
-            Date</div>
-          <p className="font-semibold">{new Date().toLocaleDateString()}</p>
-        </div>
-
-      </div>
+      <GameInfoDisplay
+        color={gameStatusDiv.color}
+        text={gameStatusDiv.text}
+        id={id}
+      />
 
       <UserDisplay
         color={userColor}
