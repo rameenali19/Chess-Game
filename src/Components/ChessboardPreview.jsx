10c1e45aab4b461abe61b18d1cfbcee3f014a6df
rameenali19 <rameenali1910@gmail.com>
2026-08-19 01:52:07 +0500

    used chessboard preview in mode selection page

--- src/Components/ChessboardPreview.jsx
+++ src/Components/ChessboardPreview.jsx
@@ -1,4 +1,24 @@
-function ChessboardPreview() {
+import Button from "./Button";
 
+function ChessboardPreview() {
+  return (
+    <div>
+      <div className="flex items-center gap-3 w-full mb-4">
+        <Button
+          text="How To Play"
+          variant="sideBarBlue"
+          textSize="small"
+          fontWeight="medium"
+          className="px-2 gap-2 justify-center h-8  flex items-center"
+          image="/book.png"
+          imageText="book image"
+          imageStyling="w-5 h-5" />
+        <div className="text-gray-700 font-medium">Move the piece to checkmate your opponent</div>
+      </div>
+      <img className=" mb-1 p-3 border-2 border-[#C7A97A] rounded-lg w-115"
+        src="/chessboard.png" alt="chessboard image"
+      ></img>
+    </div>
+  )
 }
 export default ChessboardPreview;
--- src/Pages/ModeSelectionPage.jsx
+++ src/Pages/ModeSelectionPage.jsx
@@ -12,6 +12,7 @@ import ModeSelectionCards from "../components/ModeSelectionCards"
 import { Navigate } from "react-router-dom";
 import Button from "../components/Button";
 import GameModeSelection from "../components/GameModeSelection";
+import ChessboardPreview from "../components/ChessboardPreview";
 
 function ModeSelectionPage() {
 
@@ -56,23 +57,7 @@ function ModeSelectionPage() {
         transition={{ duration: 0.4, ease: "easeOut" }}
         className="flex justify-center gap-8 items-center translate-y-6 ">
 
-        <div>
-          <div className="flex items-center gap-3 w-full mb-4">
-            <Button
-              text="How To Play"
-              variant="sideBarBlue"
-              textSize="small"
-              fontWeight="medium"
-              className="px-2 gap-2 justify-center h-8  flex items-center"
-              image="/book.png"
-              imageText="book image"
-              imageStyling="w-5 h-5" />
-            <div className="text-gray-700 font-medium">Move the piece to checkmate your opponent</div>
-          </div>
-          <img className=" mb-1 p-3 border-2 border-[#C7A97A] rounded-lg w-115"
-            src="/chessboard.png" alt="chessboard image"
-          ></img>
-        </div>
+        <ChessboardPreview />
 
         <GameModeSelection
           setMode={setMode}
