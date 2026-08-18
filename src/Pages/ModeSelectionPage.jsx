77276881dc9d9e3e2cbe8ea749aac8a63a5b695e
rameenali19 <rameenali1910@gmail.com>
2026-08-19 01:47:58 +0500

    created GameModeSelection

--- src/Pages/ModeSelectionPage.jsx
+++ src/Pages/ModeSelectionPage.jsx
@@ -74,20 +74,7 @@ function ModeSelectionPage() {
         </div>
 
 
-        <div>
-          <div className="flex gap-2 items-center justify-center">
-            <img className="object-contain w-10 h-8"
-              src="/trophy.png" alt="trophy image" >
-            </img>
-            <h1 className="font-cormorant text-3xl font-bold flex text-[#113447] ">
-              Choose a Game Mode</h1>
-          </div>
-          <div className="flex flex-col gap-3 mt-3" >
-            <ModeSelectionCards
-              setMode={setMode} />
-          </div>
-        </div>
-
+       
       </motion.div>
       {
         loginCondition && (
