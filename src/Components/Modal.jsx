0f5816369eea2adda33b333e0e083cd09f278cc3
rameenali19 <rameenali1910@gmail.com>
2026-08-19 20:04:14 +0500

    removed motion from modal and added css class

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,22 +1,16 @@
-import { motion } from "framer-motion";
-
 function Modal({ open, className, children }) {
   if (open) {
 
     return (
       <div className="fixed inset-0 flex justify-center items-center bg-[#3E2C20]/25 z-50">
 
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.4, ease: "easeOut" }}
-          className={`border border-[#E8DCC7] shadow-2xl rounded-xl
+        <div className={`page border border-[#E8DCC7] shadow-2xl rounded-xl
          shadow-[rgba(23,56,74,0.15)] bg-[url('/orangebg.png')]
          bg-center bg-cover ${className}`}>
 
           {children}
 
-        </motion.div>
+        </div>
       </div>
     )
 
