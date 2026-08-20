7c61072d8c4ae3e531437675d61c008842e78157
rameenali19 <rameenali1910@gmail.com>
2026-08-20 22:35:39 +0500

    changing imports in Icon and inisde modal

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,3 +1,4 @@
+import orangeBg from "../assets/Background/orangebg.png";
 function Modal({ open, className, children }) {
   if (open) {
 
@@ -5,17 +6,14 @@ function Modal({ open, className, children }) {
       <div className="fixed inset-0 flex justify-center items-center bg-[#3E2C20]/25 z-50">
 
         <div className={`page border border-[#E8DCC7] shadow-2xl rounded-xl
-         shadow-[rgba(23,56,74,0.15)] bg-[url('/orangebg.png')]
-         bg-center bg-cover ${className}`}>
-
+         shadow-[rgba(23,56,74,0.15)] bg-center bg-cover ${className}`}
+          style={{ backgroundImage: `url(${orangeBg})` }}
+        >
           {children}
-
         </div>
       </div>
     )
-
   }
   else return null
-
 }
 export default Modal;
