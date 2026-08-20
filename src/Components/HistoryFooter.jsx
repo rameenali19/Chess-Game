779962028d51ee7ca74d02ef67c24295e1adbf20
rameenali19 <rameenali1910@gmail.com>
2026-08-20 17:10:19 +0500

    using Icons inside HistoryFooter

--- src/Components/HistoryFooter.jsx
+++ src/Components/HistoryFooter.jsx
@@ -1,12 +1,14 @@
+import Icon from "./Icon";
 function HistoryFooter() {
 
   return (
     <div className="font-inter text-lg text-[#17384A] bg-[#FFF7EA] shadow-sm border 
         border-[#E8DCC7] px-6 hover:shadow-md hover:-translate-y-0.5 transition rounded-lg
-         h-15 w-full flex items-center justify-center">
-      <img className="object-contain w-12 h-8"
-        src="/bulb.png" alt="bulb image"
-      ></img>
+         h-15 w-full flex items-center justify-center gap-1">
+      <Icon
+        name="bulb"
+        width="17"
+      />
       <div>
         Continue unfinished games or review completed matches
       </div>
