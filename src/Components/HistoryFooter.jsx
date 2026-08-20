ea0bd0dbecb6cf6a5a989938da0910421e3acdcb
rameenali19 <rameenali1910@gmail.com>
2026-08-20 20:20:35 +0500

    changed width in history footer

--- src/Components/HistoryFooter.jsx
+++ src/Components/HistoryFooter.jsx
@@ -4,10 +4,10 @@ function HistoryFooter() {
   return (
     <div className="font-inter text-lg text-[#17384A] bg-[#FFF7EA] shadow-sm border 
         border-[#E8DCC7] px-6 hover:shadow-md hover:-translate-y-0.5 transition rounded-lg
-         h-15 w-full flex items-center justify-center gap-1">
+         h-15 w-full flex items-center justify-center">
       <Icon
         name="bulb"
-        width="17"
+        className="w-12 h-8"
       />
       <div>
         Continue unfinished games or review completed matches
