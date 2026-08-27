a6432d37a131719927140e046ac07896b7254981
rameenali19 <rameenali1910@gmail.com>
2026-08-27 18:14:18 +0500

    testing sliding side panel

--- src/Components/SidePanel.jsx
+++ src/Components/SidePanel.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import SidePanelHeader from "./SidePanelHeader";
 import SidePanelNavigation from "./SidePanelNavigation";
+import Icon from "./Icon";
 
 function SidePanel() {
   const [open, setOpen] = useState(true)
@@ -9,10 +10,27 @@ function SidePanel() {
       {open && (
         <nav className=" fixed left-0 top-0 w-45 h-screen bg-[#17384A]
      text-white flex flex-col shadow-xl z-50">
-          <SidePanelHeader />
+          <SidePanelHeader
+            setOpen={setOpen}
+          />
           <SidePanelNavigation />
         </nav>
       )}
+
+      {!open && (
+        <nav className=" fixed left-0 top-0 w-15 h-screen bg-[#17384A]
+     text-white shadow-xl z-50">
+          <div className="py-4 px-2 hover:cursor-pointer"
+            onClick={() => { setOpen(true) }}
+          >
+            <Icon
+              name="logoChess"
+              className="h-9"
+            />
+          </div>
+        </nav>
+      )
+      }
     </>
   )
 }
--- src/Components/SidePanelHeader.jsx
+++ src/Components/SidePanelHeader.jsx
@@ -1,11 +1,15 @@
 import Icon from "./Icon";
-function SidePanelHeader() {
+function SidePanelHeader({ setOpen }) {
   return (
     <div className="flex items-center py-4 px-2 gap-1">
-      <Icon
-        name="logoChess"
-        className="h-9"
-      />
+      <div onClick={() => { setOpen(false) }}
+        className=" hover:cursor-pointer"
+      >
+        <Icon
+          name="logoChess"
+          className="h-9"
+        />
+      </div>
       <div className="font-cormorant text-2xl">
         <h1>Checkmate</h1>
       </div>
