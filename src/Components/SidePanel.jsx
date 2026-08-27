b889e1ad123449e3a7552b4f0ec039d01ecf8283
rameenali19 <rameenali1910@gmail.com>
2026-08-27 18:02:05 +0500

    testing sliding sidebar

--- src/Components/SidePanel.jsx
+++ src/Components/SidePanel.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import SidePanelHeader from "./SidePanelHeader";
 import SidePanelNavigation from "./SidePanelNavigation";
 
 function SidePanel() {
-
+  const [open, setOpen] = useState(true)
   return (
-    <nav className=" fixed left-0 top-0 w-45 h-screen bg-[#17384A]
+    <>
+      {open && (
+        <nav className=" fixed left-0 top-0 w-45 h-screen bg-[#17384A]
      text-white flex flex-col shadow-xl z-50">
-      <SidePanelHeader />
-      <SidePanelNavigation />
-    </nav>
+          <SidePanelHeader />
+          <SidePanelNavigation />
+        </nav>
+      )}
+    </>
   )
 }
 export default SidePanel;
