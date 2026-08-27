9e2caabf82962d350cef554949beee7fc5d2707f
rameenali19 <rameenali1910@gmail.com>
2026-08-27 18:25:47 +0500

    adjusting width

--- src/Components/SidePanel.jsx
+++ src/Components/SidePanel.jsx
@@ -7,7 +7,7 @@ function SidePanel() {
   const [open, setOpen] = useState(true)
   return (
     <nav className={`fixed left-0 top-0 h-screen bg-[#17384A]
-     text-white flex flex-col shadow-xl z-50 ${open ? "w-45" : "w-15"}`}>
+     text-white flex flex-col shadow-xl z-50 ${open ? "w-45" : "w-12"}`}>
       {open && (
         <>
           <SidePanelHeader
