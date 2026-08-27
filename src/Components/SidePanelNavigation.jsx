d21157d8f7e674c486a6867a46c6bed224f29b7a
rameenali19 <rameenali1910@gmail.com>
2026-08-27 21:43:50 +0500

    removed space

--- src/Components/SidePanelNavigation.jsx
+++ src/Components/SidePanelNavigation.jsx
@@ -29,7 +29,7 @@ function SidePanelNavigation({ open }) {
   ]
   return (
     <div className={`flex flex-col mt-3 gap-2 px-2 font-inter text-sm 
-    ${!open && "scale-0"} ${open && "  duration-500"}`}>
+    ${!open && "scale-0"} ${open && "duration-500"}`}>
       {
         sidePanelComponents.map((panel) => {
           return (
