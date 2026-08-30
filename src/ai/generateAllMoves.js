3a57a4c0d89bd89214c591270362e19fc412a5a6
rameenali19 <rameenali1910@gmail.com>
2026-08-30 16:33:33 +0500

    basic structure of generateAllMoves.js

---
+++ src/ai/generateAllMoves.js
@@ -0,0 +1,7 @@
+import { generateMoves } from "../chess/generateMoves";
+import { movePiece } from "../chess/movePiece";
+import { isKingInCheck } from "../chess/isKingInCheck";
+
+export function generateAllMoves(board, color, enpassant) {
+
+}
