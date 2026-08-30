9519b3434b9c5898288063ad6e4c63558e003b31
rameenali19 <rameenali1910@gmail.com>
2026-08-30 19:57:12 +0500

    evaluate board mechanism

--- src/ai/evaluateBoard.js
+++ src/ai/evaluateBoard.js
@@ -6,3 +6,18 @@ const pieceValue = {
   Queen: 900,
   King: 20000
 }
+
+export function evaluateBoard(board) {
+  let score = 0;
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      const piece = board[row][col];
+      if (piece === ".") continue;
+      const value = pieceValue[piece.type];
+      if (piece.color === "White")
+        score = score + value;
+      else score = score - value;
+    }
+  }
+  return score;
+}
