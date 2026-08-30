5cf359524e3216282d81ca56f97b735a14ffa95b
rameenali19 <rameenali1910@gmail.com>
2026-08-30 23:56:42 +0500

    basic minimax function

--- src/ai/minimax.js
+++ src/ai/minimax.js
@@ -1,3 +1,7 @@
 import { generateAllMoves } from "./generateAllMoves";
 import { evaluateBoard } from "./evaluateBoard";
 import { movePiece } from "../chess/movePiece";
+
+export function minimax(board, depth, maximizingPlayer, currentColor, aiColor, enPassant) {
+
+}
