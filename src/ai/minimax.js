b3751558b3b83542a14cc6a04d638e1ea664da91
rameenali19 <rameenali1910@gmail.com>
2026-08-31 00:28:45 +0500

    changing parameter

--- src/ai/minimax.js
+++ src/ai/minimax.js
@@ -23,7 +23,7 @@ export function minimax(board, depth, maximizingPlayer, currentColor, aiColor, e
 
       const nextColor = currentColor === "White" ? "Black" : "White";
 
-      const score = minimax(board,
+      const score = minimax(newBoard,
         depth - 1,
         false,
         nextColor,
@@ -31,6 +31,31 @@ export function minimax(board, depth, maximizingPlayer, currentColor, aiColor, e
         enPassant)
 
       bestScore = Math.max(bestScore, score)
+    }
+    return bestScore;
+  }
+
+  else {
+    let bestScore = Infinity;
+
+    for (const move of moves) {
+      const newBoard = movePiece(move.destination.row,
+        move.destination.col,
+        move.piece,
+        board,
+        move.destination.castle,
+        move.destination.enPassant)
+
+      const nextColor = currentColor === "White" ? "Black" : "White";
+
+      const score = minimax(newBoard,
+        depth - 1,
+        true,
+        nextColor,
+        aiColor,
+        enPassant)
+
+      bestScore = Math.min(bestScore, score)
     }
     return bestScore;
   }
