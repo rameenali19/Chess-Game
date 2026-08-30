470b5ade8272d1e7e928606dee5ffb4767d42d4f
rameenali19 <rameenali1910@gmail.com>
2026-08-30 17:41:23 +0500

    generateAllMoves logic

--- src/ai/generateAllMoves.js
+++ src/ai/generateAllMoves.js
@@ -3,5 +3,21 @@ import { movePiece } from "../chess/movePiece";
 import { isKingInCheck } from "../chess/isKingInCheck";
 
 export function generateAllMoves(board, color, enpassant) {
+  const legalMoves = [];
 
+  for (let row = 0; row < 8; row++) {
+
+    for (let col = 0; col < 8; col++) {
+      const piece = board[row][col];
+      if (piece === ".") continue;
+      if (piece.color !== color) continue;
+
+      const piecePosition = {
+        ...piece,
+        row,
+        col
+      }
+      
+    }
+  }
 }
