2839181c64ad82b8bc87296dc1f92ab65b558d78
rameenali19 <rameenali1910@gmail.com>
2026-08-31 01:14:57 +0500

    changing return value at depth =0

--- src/ai/minimax.js
+++ src/ai/minimax.js
@@ -5,7 +5,9 @@ import { movePiece } from "../chess/movePiece";
 export function minimax(board, depth, maximizingPlayer, currentColor, aiColor, enPassant) {
 
   if (depth === 0) {
-    return evaluateBoard(board, aiColor)
+    return {
+      score: evaluateBoard(board, aiColor)
+    }
   }
 
   const moves = generateAllMoves(board, currentColor, enPassant)
