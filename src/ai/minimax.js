1967ca67d12d823239d5a5f0111f5e98a28f9f02
rameenali19 <rameenali1910@gmail.com>
2026-08-30 23:57:35 +0500

    ADDING EVALUATE BOARD IN MINIMAX

--- src/ai/minimax.js
+++ src/ai/minimax.js
@@ -4,4 +4,7 @@ import { movePiece } from "../chess/movePiece";
 
 export function minimax(board, depth, maximizingPlayer, currentColor, aiColor, enPassant) {
 
+  if (depth === 0) {
+    return evaluateBoard(board, aiColor)
+  }
 }
