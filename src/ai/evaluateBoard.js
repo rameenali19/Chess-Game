81f5718499426014c798b6a9d5befa1f3bbe4d69
rameenali19 <rameenali1910@gmail.com>
2026-08-31 22:46:21 +0500

    changing board evaluation

--- src/ai/evaluateBoard.js
+++ src/ai/evaluateBoard.js
@@ -1,23 +1,155 @@
 const pieceValue = {
-  Pawn: 100,
-  Knight: 320,
-  Bishop: 330,
-  Rook: 500,
-  Queen: 900,
-  King: 20000
+  Pawn: 10,
+  Knight: 30,
+  Bishop: 30,
+  Rook: 50,
+  Queen: 90,
+  King: 900
+};
+
+
+const pawnEvalWhite = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0],
+  [1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0],
+  [0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5],
+  [0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0],
+  [0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5],
+  [0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5],
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+];
+
+const pawnEvalBlack = [...pawnEvalWhite].reverse();
+
+
+const knightEval = [
+  [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+  [-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0],
+  [-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0],
+  [-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0],
+  [-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0],
+  [-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0],
+  [-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0],
+  [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+];
+
+
+const bishopEvalWhite = [
+  [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+  [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0],
+  [-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0],
+  [-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0],
+  [-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0],
+  [-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0],
+  [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+];
+
+const bishopEvalBlack = [...bishopEvalWhite].reverse();
+
+
+const rookEvalWhite = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0]
+];
+
+const rookEvalBlack = [...rookEvalWhite].reverse();
+
+
+const evalQueen = [
+  [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+  [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+  [-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+  [0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+  [-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+];
+
+
+const kingEvalWhite = [
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+  [-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+  [2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0],
+  [2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0]
+];
+
+const kingEvalBlack = [...kingEvalWhite].reverse();
+
+function getPositionValue(piece, row, col) {
+
+  switch (piece.type) {
+
+    case "Pawn":
+      return piece.color === "White"
+        ? pawnEvalWhite[row][col]
+        : pawnEvalBlack[row][col];
+
+    case "Knight":
+      return knightEval[row][col];
+
+    case "Bishop":
+      return piece.color === "White"
+        ? bishopEvalWhite[row][col]
+        : bishopEvalBlack[row][col];
+
+    case "Rook":
+      return piece.color === "White"
+        ? rookEvalWhite[row][col]
+        : rookEvalBlack[row][col];
+
+    case "Queen":
+      return evalQueen[row][col];
+
+    case "King":
+      return piece.color === "White"
+        ? kingEvalWhite[row][col]
+        : kingEvalBlack[row][col];
+
+    default:
+      return 0;
+  }
 }
 
 export function evaluateBoard(board, aiColor) {
+
   let score = 0;
+
   for (let row = 0; row < 8; row++) {
+
     for (let col = 0; col < 8; col++) {
+
       const piece = board[row][col];
+
       if (piece === ".") continue;
-      const value = pieceValue[piece.type];
-      if (piece.color === aiColor)
-        score = score + value;
-      else score = score - value;
+
+      const materialValue = pieceValue[piece.type];
+
+      const positionValue =
+        getPositionValue(piece, row, col);
+
+      const totalValue =
+        materialValue + positionValue;
+
+      if (piece.color === aiColor) {
+        score += totalValue;
+      }
+      else {
+        score -= totalValue;
+      }
     }
   }
+
   return score;
 }
