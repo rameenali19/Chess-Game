e2034711184ee8c4e69cd00b49124df3960aeba1
rameenali19 <rameenali1910@gmail.com>
2026-09-02 01:34:43 +0500

    adding levels

--- src/components/DifficultyLevel.jsx
+++ src/components/DifficultyLevel.jsx
@@ -1,4 +1,50 @@
 function DifficultyLevel() {
 
+  const levels = [
+    {
+      image: "greenPawn",
+      title: "Easy",
+      line1: "Best for beginners",
+      line2: "and casual play",
+      divColor: "#E8F0E2",
+      textColor: "#35843C",
+      borderColor: "#35843C",
+      mode: "singleplayer",
+    },
+
+    {
+      image: "yellowKnight",
+      title: "Medium",
+      line1: "A balanced challange",
+      line2: "for improving players",
+      divColor: "#FFF1CC",
+      textColor: "#D99A00",
+      borderColor: "#D99A00",
+      mode: "multiplayer",
+    },
+
+    {
+      image: "orangeRook",
+      title: "Hard",
+      line1: "For experienced players",
+      line2: "looking for a challenge",
+      divColor: "#FCE8D4",
+      textColor: "#E67E00",
+      borderColor: "#E67E00",
+      mode: "join",
+    },
+    {
+      image: "redQueens",
+      title: "Expert",
+      line1: "For advanced players",
+      line2: "seeking the ultimate test",
+      divColor: "#FBE0E0",
+      textColor: "#D9413A",
+      borderColor: "#D9413A",
+      mode: "ai",
+    }
+  ]
+
+
 }
 export default DifficultyLevel;
