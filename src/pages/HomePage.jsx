766e857dae5ce5bf947b68bf216f395d55340574
rameenali19 <rameenali1910@gmail.com>
2026-09-04 02:24:55 +0500

    making new file HomePageLogo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,22 @@
+import HomePageLogo from "../components/HomePageLogo"
+
 function HomePage() {
   return (
-    <>
 
-    </>
+    <div flex flex-col>
+      <div>
+        <HomePageLogo />
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
+    </div>
+
   )
 }
 export default HomePage
