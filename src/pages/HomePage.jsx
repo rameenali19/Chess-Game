b0714bcab6d4d3e958718b16cbcbed3fc79e3540
rameenali19 <rameenali1910@gmail.com>
2026-09-04 03:11:05 +0500

    adding css class

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import HomePageLogo from "../components/HomePageLogo"
 function HomePage() {
   return (
 
-    <div className="flex flex-col px-10 items-center justify-center min-h-screen">
+    <div className="page flex flex-col px-10 items-center justify-center min-h-screen">
       <div>
         <HomePageLogo />
       </div>
