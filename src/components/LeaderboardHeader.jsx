d5e869e3fa32168a86cdfa02ae7ccfbd5d4251db
rameenali19 <rameenali1910@gmail.com>
2026-09-05 03:17:13 +0500

    using navigate inside the leaderboardheader file

--- src/components/LeaderboardHeader.jsx
+++ src/components/LeaderboardHeader.jsx
@@ -1,7 +1,9 @@
 import Button from "./Button";
 import Icon from "./Icon";
+import { useNavigate } from "react-router-dom";
 
 function LeaderboardHeader() {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col gap-2">
       <div className="flex justify-between items-center mt-5">
