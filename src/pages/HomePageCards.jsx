a78ced88f5ca5d6b2c124b28d505d9fba0770807
rameenali19 <rameenali1910@gmail.com>
2026-09-04 22:58:37 +0500

    changing heading name

--- src/pages/HomePageCards.jsx
+++ src/pages/HomePageCards.jsx
@@ -37,7 +37,8 @@ function HomePageCards() {
 
   return (
     <div className="flex flex-col gap-7 w-full items-center">
-      <h1 className="font-cormorant text-[#17384A] text-4xl">Why Play on Checkmate?</h1>
+      <h1 className="font-cormorant text-[#17384A] text-4xl">
+        Features to Explore</h1>
       <div className="flex gap-6">
         {
           cards.map((feature) => {
