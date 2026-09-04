6694eda3a823a576ba6c46bc3c7a2182355bbd53
rameenali19 <rameenali1910@gmail.com>
2026-09-05 03:12:03 +0500

    importing leaderboardheader file

--- src/components/LeaderboardHeader.jsx
+++ src/components/LeaderboardHeader.jsx
@@ -1,4 +1,25 @@
-function LeaderboardHeader() {
+import Button from "./Button";
+import Icon from "./Icon";
 
+function LeaderboardHeader() {
+  return (
+    <div className="flex justify-between items-center mt-5 mb-5">
+      <div className="text-3xl font-semibold text-[#17384A] font-playfair flex items-center gap-2">
+        <Icon
+          name="trophy"
+          className="w-10"
+        />
+        <h1>Leaderboard</h1>
+      </div>
+      <Button
+        text="+ New Game"
+        variant="primary"
+        textSize="normal"
+        fontWeight="normal"
+        onClick={() => navigate("/mode-selection")}
+        className="w-35 h-10"
+      />
+    </div>
+  )
 }
 export default LeaderboardHeader;
--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,6 +1,10 @@
+import LeaderboardHeader from "../components/LeaderboardHeader";
+
 function LeaderboardPage() {
   return (
     <main className="page flex gap-5 flex-col px-8 mb-2">
+
+      <LeaderboardHeader />
 
     </main>
   )
