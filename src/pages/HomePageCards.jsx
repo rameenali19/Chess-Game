4645fbb7909a54f29a248febe4bbe13a34cd7126
rameenali19 <rameenali1910@gmail.com>
2026-09-04 13:46:49 +0500

    creating const for map

--- src/pages/HomePageCards.jsx
+++ src/pages/HomePageCards.jsx
@@ -1,4 +1,37 @@
 function HomePageCards() {
-
+  const cards = [
+    {
+      image: "orangeButton",
+      heading: "Play Your Way",
+      line1: "Choose from single player,",
+      line2: "multiplayer, or challange",
+      line3: "our AI bot",
+      textColor: "#35843C"
+    },
+    {
+      image: "orangeButton",
+      heading: "Create & Connect",
+      line1: "Create a game and invite",
+      line2: "your friends or join others",
+      line3: "around the world",
+      textColor: "#E67E00"
+    },
+    {
+      image: "orangeButton",
+      heading: "Track & Improve",
+      line1: "View your game history,",
+      line2: "learn from your moves,",
+      line3: "and improve everyday",
+      textColor: "#D9413A"
+    },
+    {
+      image: "orangeButton",
+      heading: "Built for Everyone",
+      line1: "Whether you are a beginner",
+      line2: "or a pro, Checkmate is",
+      line3: "for you",
+      textColor: "#E67E00"
+    }
+  ]
 }
 export default HomePageCards;
