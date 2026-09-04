dae96cb1903a281368a734bed1f2bf4343fb5e61
rameenali19 <rameenali1910@gmail.com>
2026-09-04 23:41:16 +0500

    changing heading color

--- src/components/HomePageStats.jsx
+++ src/components/HomePageStats.jsx
@@ -14,7 +14,7 @@ function HomePageStats() {
       />
 
       <div className="flex flex-col gap-3">
-        <h1 className="font-cormorant text-[#443496] text-3xl font-semibold">
+        <h1 className="font-cormorant text-[#17384A] text-3xl font-semibold">
           Curious about your performance?</h1>
         <div className="font-inter text-xs text-gray-700 flex flex-col gap-1 tracking-wider">
           <h1>View your stats, win rate, ranking and more.</h1>
