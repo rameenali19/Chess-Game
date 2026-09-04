d88393aa0714082369abb427da177efedc7571df
rameenali19 <rameenali1910@gmail.com>
2026-09-05 04:00:51 +0500

    adding guestid guard

--- src/components/StatsOverview.jsx
+++ src/components/StatsOverview.jsx
@@ -1,4 +1,18 @@
+import { useEffect, useState, useContext } from "react";
+import ApiChess from "../api/apiChess";
+import { UserContext } from "../context/UserContext";
+
 function StatsOverview() {
+  const { guestId } = useContext(UserContext);
+
+  useEffect(() => {
+    async function getAllGames() {
+      const game = ApiChess.getAPI();
+      const data = await game.getAllGames(1, 10, guestId);
+      console.log(data)
+    }
+    getAllGames();
+  }, [])
 
   return (
     <div className="flex flex-col">
--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,11 +1,14 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import LeaderboardHeader from "../components/LeaderboardHeader";
 import LeaderboardNavbar from "../components/LeaderboardNavbar";
 import MyStats from "../components/MyStats";
 import GlobalStats from "../components/GlobalStats";
+import { UserContext } from "../context/UserContext";
 
 function LeaderboardPage() {
   const [status, setStatus] = useState("myStats")
+  const { guestId } = useContext(UserContext);
+
   return (
     <main className="page flex gap-5 flex-col px-8 mb-2">
 
@@ -16,13 +19,13 @@ function LeaderboardPage() {
       />
 
       {
-        status === "myStats" && (
+        status === "myStats" && guestId && (
           <MyStats />
         )
       }
 
       {
-        status === "globalStats" && (
+        status === "globalStats" && guestId && (
           <GlobalStats />
         )
       }
