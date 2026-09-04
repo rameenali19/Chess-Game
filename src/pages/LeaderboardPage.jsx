d9992b97e56935cd003ed665a47c2716f5909578
rameenali19 <rameenali1910@gmail.com>
2026-09-05 03:21:34 +0500

    passing prop to LeaderboardNavbar

--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import LeaderboardHeader from "../components/LeaderboardHeader";
 import LeaderboardNavbar from "../components/LeaderboardNavbar";
 import MyStats from "../components/MyStats";
 
 function LeaderboardPage() {
+  const [fiter, setFilter] = useState(null)
   return (
     <main className="page flex gap-5 flex-col px-8 mb-2">
 
       <LeaderboardHeader />
 
-      <LeaderboardNavbar />
+      <LeaderboardNavbar
+        setFilter={setFilter}
+      />
 
     </main>
   )
