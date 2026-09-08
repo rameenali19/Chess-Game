7ab1194a1a971a7114b8e54e5306bd7d94587f6b
rameenali19 <rameenali1910@gmail.com>
2026-09-08 11:23:35 +0500

    changed font weight

--- src/components/SidePanelHeader.jsx
+++ src/components/SidePanelHeader.jsx
@@ -1,12 +1,12 @@
 import Icon from "./Icon";
 function SidePanelHeader({ open }) {
   return (
-    <div className="flex items-center py-4 px-6 gap-2">
+    <div className="flex items-center py-3 px-6 gap-2">
       <Icon
         name="logoChess"
         className="h-9"
       />
-      <div className={`font-playfair text-2xl ${!open && "scale-0"}
+      <div className={`font-playfair text-2xl ${!open && "scale-0"} font-medium 
        ${open && "  duration-500"}`}>
         <h1>Checkmate</h1>
       </div>
