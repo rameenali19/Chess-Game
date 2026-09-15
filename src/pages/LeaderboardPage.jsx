3faeed4602e8d5eb58a25eff2f6ed2f640a9b095
rameenali19 <rameenali1910@gmail.com>
2026-09-15 12:00:57 +0500

    removing guestId condition in LeaderboardPage

--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,13 +1,11 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import LeaderboardHeader from "../components/LeaderboardHeader";
 import LeaderboardNavbar from "../components/LeaderboardNavbar";
 import MyStats from "../components/MyStats";
 import GlobalStats from "../components/GlobalStats";
-import { UserContext } from "../context/UserContext";
 
 function LeaderboardPage() {
   const [status, setStatus] = useState("myStats")
-  const { guestId } = useContext(UserContext);
 
   return (
     <main className="page flex gap-5 flex-col px-8 mb-2">
@@ -19,13 +17,13 @@ function LeaderboardPage() {
       />
 
       {
-        status === "myStats" && guestId && (
+        status === "myStats" && (
           <MyStats />
         )
       }
 
       {
-        status === "globalStats" && guestId && (
+        status === "globalStats" && (
           <GlobalStats />
         )
       }
