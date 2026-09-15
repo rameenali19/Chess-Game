4a7b4c77a77b7fb3ecdabdfca867129cebc89552
rameenali19 <rameenali1910@gmail.com>
2026-09-15 13:18:19 +0500

    making states for data display

--- src/components/StatsOverview.jsx
+++ src/components/StatsOverview.jsx
@@ -4,11 +4,17 @@ import { UserContext } from "../context/UserContext";
 
 function StatsOverview() {
   const { guestId } = useContext(UserContext);
+  const [totalGames, setTotalGames] = useState(null);
+  const [totalWin, setTotalWin] = useState(null)
+  const [totalLost, setTotalLost] = useState(null)
+  const [totalDraw, setTotalDraw] = useState(null)
 
   useEffect(() => {
     async function getAllFinishedGames() {
       const game = ApiChess.getAPI();
       const data = await game.getAllFinishedGames(guestId);
+      setTotalGames(data.length)
+      
     }
     getAllFinishedGames();
   }, [guestId])
