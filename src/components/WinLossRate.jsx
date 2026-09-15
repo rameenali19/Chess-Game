9b26398d3fb02d59f8e5e293067a819b72549d1a
rameenali19 <rameenali1910@gmail.com>
2026-09-15 15:58:02 +0500

    adding states and one array for mapping over

--- src/components/WinLossRate.jsx
+++ src/components/WinLossRate.jsx
@@ -1,4 +1,29 @@
-function WinLossRate({ totalGames, totalWin, totalLost }) {
+import { useState } from "react";
 
+function WinLossRate({ totalGames, totalWin, totalLost }) {
+  const [winRate, setWinRate] = useState(0)
+  const [lostRate, setLostRate] = useState(0)
+  const win = (totalGames / totalWin) * 100;
+  setWinRate(win)
+  const lost = (totalGames / totalLost) * 100;
+  setLostRate(lost)
+  const circle = [
+    {
+      text: "Win Rate",
+      fillerWord: "wins",
+      rate: winRate,
+      color: "#35843C"
+    },
+    {
+      text: "Loss Rate",
+      fillerWord: "losses",
+      rate: lostRate,
+      color: "#D9413A"
+    }
+  ]
+
+  return (
+
+  )
 }
 export default WinLossRate;
