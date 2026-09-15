6f3cd9720283f6d38316e8d5f97140c2b019b8df
rameenali19 <rameenali1910@gmail.com>
2026-09-15 13:24:25 +0500

    filtering data and storing them in states

--- src/components/StatsOverview.jsx
+++ src/components/StatsOverview.jsx
@@ -8,21 +8,36 @@ function StatsOverview() {
   const [totalWin, setTotalWin] = useState(null)
   const [totalLost, setTotalLost] = useState(null)
   const [totalDraw, setTotalDraw] = useState(null)
+  const statDivs =[
+    {
+      
+    }
+  ]
 
   useEffect(() => {
     async function getAllFinishedGames() {
       const game = ApiChess.getAPI();
       const data = await game.getAllFinishedGames(guestId);
       setTotalGames(data.length)
-      
+      const won = data.filter(
+        game => game.winner === game.player_color
+      ).length;
+      setTotalWin(won);
+      const draw = data.filter(
+        game => game.winner === "Draw"
+      ).length;
+      setTotalWin(draw);
+
+      const lost = totalGames - won - draw;
+      setTotalLost(lost)
     }
     getAllFinishedGames();
   }, [guestId])
 
   return (
     <div className="flex flex-col">
-      <div className="font-playfair text-[#17384A] font-semibold text-2xl">
-        Your Statistics
+      <div className="font-playfair text-[#17384A] flex">
+        
       </div>
     </div>
   )
