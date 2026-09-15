7d92f91fa422afea7ecfbc0c02f0d05bcf6dd9d3
rameenali19 <rameenali1910@gmail.com>
2026-09-15 15:16:38 +0500

    shifting api calling to mystats page

--- src/components/MyStats.jsx
+++ src/components/MyStats.jsx
@@ -1,6 +1,21 @@
 import StatsOverview from "./StatsOverview";
+import { useEffect, useState, useContext } from "react";
+import ApiChess from "../api/apiChess";
+import { UserContext } from "../context/UserContext";
 
-function MyStats({ games }) {
+
+function MyStats() {
+  const { guestId } = useContext(UserContext);
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    async function getAllFinishedGames() {
+      const game = ApiChess.getAPI();
+      const data = await game.getAllFinishedGames(guestId);
+      setGames(data)
+    }
+    getAllFinishedGames();
+  }, [guestId])
 
   return (
     <div className="flex flex-col">
--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,25 +1,13 @@
-import { useState, useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 import LeaderboardHeader from "../components/LeaderboardHeader";
 import LeaderboardNavbar from "../components/LeaderboardNavbar";
 import ApiChess from "../api/apiChess";
 import MyStats from "../components/MyStats";
 import GlobalStats from "../components/GlobalStats";
-import { UserContext } from "../context/UserContext";
 
 function LeaderboardPage() {
 
   const [status, setStatus] = useState("myStats")
-  const { guestId } = useContext(UserContext);
-  const [games, setGames] = useState([])
-
-  useEffect(() => {
-    async function getAllFinishedGames() {
-      const game = ApiChess.getAPI();
-      const data = await game.getAllFinishedGames(guestId);
-      setGames(data)
-    }
-    getAllFinishedGames();
-  }, [guestId])
 
   return (
     <main className="page flex gap-5 flex-col px-8 mb-2">
@@ -32,9 +20,7 @@ function LeaderboardPage() {
 
       {
         status === "myStats" && (
-          <MyStats
-            games={games}
-          />
+          <MyStats />
         )
       }
 
