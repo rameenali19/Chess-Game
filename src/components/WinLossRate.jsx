a3bdb385f6c360350c6ca0c29d6bce78657c3c48
rameenali19 <rameenali1910@gmail.com>
2026-09-15 16:36:30 +0500

    styling the WinLossRate page

--- src/components/WinLossRate.jsx
+++ src/components/WinLossRate.jsx
@@ -7,43 +7,54 @@ import {
 import "react-circular-progressbar/dist/styles.css";
 
 function WinLossRate({ totalGames, totalWin, totalLost }) {
-  const winRate = (totalGames / totalWin) * 100;
-  const lostRate = (totalGames / totalLost) * 100;
+  const winRate = (totalWin / totalGames) * 100;
+  const lostRate = (totalLost / totalGames) * 100;
 
   const circle = [
     {
       text: "Win Rate",
       fillerWord: "wins",
       rate: winRate,
-      color: "#35843C"
+      color: "#35843C",
+      value: totalWin
     },
     {
       text: "Loss Rate",
       fillerWord: "losses",
       rate: lostRate,
-      color: "#D9413A"
+      color: "#D9413A",
+      value: totalLost
     }
   ]
 
   return (
-    <div className="flex gap-10">
+    <div className="flex gap-10 bg-[#FFF8EA] w-200 h-33 justify-around">
       {
         circle.map((rate) => {
           return (
             <div key={rate.text}
-              className="flex bg-[#FFF8EA]">
+              className="flex items-center rounded-lg gap-8">
 
-              <div className="w-27 h-27">
+              <div className="w-20 h-20 font-inter font-bold">
                 <CircularProgressbar
                   value={rate.rate}
                   text={`${rate.rate}%`}
-                  strokeWidth={5}
+                  strokeWidth={7}
                   styles={buildStyles({
                     textColor: rate.color,
                     pathColor: rate.color,
-                    textSize: "14px"
+                    textSize: "25px"
                   })}
                 />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <h1 className="font-playfair text-[#17384A] font-bold text-xl">
+                  {rate.text}
+                </h1>
+                <h1 className="text-xs text-gray-500 font-inter">
+                  {`${rate.value} ${rate.fillerWord} out of ${totalGames} games`}
+                </h1>
               </div>
             </div>
           )
