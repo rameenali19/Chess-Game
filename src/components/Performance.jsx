c074d888e2315d68de381ee23530c41c5ade9a93
rameenali19 <rameenali1910@gmail.com>
2026-09-16 17:03:15 +0500

    making array to map over

--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -1,6 +1,32 @@
 function Performance({ singlePlayerGames, multiplayerGames, aiGames, singlePlayerWon, multiplayerWon,
   aiWon, singlePlayerLost, multiplayerLost, aiLost
 }) {
+  const singleplayerWinRate = (singlePlayerWon / singlePlayerGames) * 100 || 0;
+  const multiplayerWinRate = (multiplayerWon / multiplayerGames) * 100 || 0;
+  const aiWinRate = (aiWon / aiGames) * 100 || 0;
 
+  const stats = [
+    {
+      mode: "Single Player",
+      games: singlePlayerGames,
+      wins: singlePlayerWon,
+      lost: singlePlayerLost,
+      rate: singleplayerWinRate
+    },
+    {
+      mode: "Multiplayer",
+      games: multiplayerGames,
+      wins: multiplayerWon,
+      lost: multiplayerLost,
+      rate: multiplayerWinRate
+    },
+    {
+      mode: "Ai Bot",
+      games: aiWon,
+      wins: singlePlayerWon,
+      lost: aiLost,
+      rate: aiWinRate
+    }
+  ]
 }
 export default Performance;
