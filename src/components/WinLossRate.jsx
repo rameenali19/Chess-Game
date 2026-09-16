8456dc3e7c06bda511f53b94fe38bd54306d7947
rameenali19 <rameenali1910@gmail.com>
2026-09-16 16:12:21 +0500

    displaying 0 in progress bar when there are no stats

--- src/components/WinLossRate.jsx
+++ src/components/WinLossRate.jsx
@@ -7,8 +7,8 @@ import {
 import "react-circular-progressbar/dist/styles.css";
 
 function WinLossRate({ totalGames, totalWin, totalLost }) {
-  const winRate = (totalWin / totalGames) * 100;
-  const lostRate = (totalLost / totalGames) * 100;
+  const winRate = (totalWin / totalGames) * 100||0;
+  const lostRate = (totalLost / totalGames) * 100||0;
 
   const circle = [
     {
