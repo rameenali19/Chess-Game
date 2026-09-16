64f61f7a28d64eb4e2bdb689650aa41ab64c15ad
rameenali19 <rameenali1910@gmail.com>
2026-09-16 17:11:18 +0500

    changing width

--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -30,7 +30,7 @@ function Performance({ singlePlayerGames, multiplayerGames, aiGames, singlePlaye
   ]
 
   return (
-    <div className="bg-[#FFF8EA] w-200 h-60 rounded-lg">
+    <div className="bg-[#FFF8EA] w-240 h-60 rounded-lg">
       {
         stats.map((values) => {
           return (
