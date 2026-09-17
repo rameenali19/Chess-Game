c6f0141a21d3142e804c2507d132ef7f013dd630
rameenali19 <rameenali1910@gmail.com>
2026-09-17 12:16:41 +0500

    modified tables :)

--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -35,48 +35,100 @@ function Performance({ singlePlayerGames, multiplayerGames, aiGames, singlePlaye
   ]
 
   return (
-    <div className="bg-[#FFF8EA] w-240 h-75 rounded-lg flex flex-col px-5 py-5 justify-around gap-5">
+    <div className="bg-[#FFF8EA] w-240 h-70 rounded-lg flex flex-col gap-5 px-5 py-5">
       <h1 className="font-playfair text-[#17384A] text-xl font-bold">Performance By Game Mode</h1>
+
       <table className="w-full font-inter">
-        <thead>
-          <tr>
-            <th>Game Type</th>
+        <thead className="border-b ">
+          <tr className="font-inter text-xs text-gray-500">
+            <th className="text-left pb-3">Game Type</th>
             <th>Games</th>
             <th>Won</th>
             <th>Lose</th>
             <th>Win Rate</th>
           </tr>
         </thead>
+
+        <tbody>
+          {
+            stats.map((values) => {
+              return (
+                <tr key={values.mode}
+                >
+                  <td className="flex items-center gap-2 pt-3">
+                    <Icon
+                      name={values.image}
+                      className="w-10 h-10"
+                    />
+                    <h1 className="font-inter text-[#17384A] text-sm font-medium">
+                      {values.mode}
+                    </h1>
+                  </td>
+                  <td className="text-center">{values.games}</td>
+                  <td className="text-center">{values.wins}</td>
+                  <td className="text-center">{values.lost}</td>
+                  <td className="text-center">{values.rate}</td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
       </table>
-      {/* <div className="flex font-inter text-xs text-gray-500 justify-between font-medium mb-4">
-        <h1>Game Type</h1>
-        <h1>Games</h1>
-        <h1>Won</h1>
-        <h1>Lose</h1>
-        <h1>Win Rate</h1>
-      </div>
-      {
-        stats.map((values) => {
-          return (
-            <div key={values.mode}
-              className="flex items-center justify-between">
-              <div className="flex items-center gap-2">
-                <Icon
-                  name={values.image}
-                  className="w-10 h-10"
-                />
-                <h1 className="font-inter text-[#17384A] text-sm font-medium">
-                  {values.mode}
-                </h1>
-              </div>
-              <h1>{values.games}</h1>
-              <h1>{values.wins}</h1>
-              <h1>{values.lost}</h1>
-            </div>
-          )
-        })
-      } */}
+
     </div>
   )
 }
 export default Performance;
+
+
+// <Table>
+//                     <TableHeader>
+//                         <TableRow>
+//                             <TableHead >Id</TableHead>
+//                             <TableHead >CLient Id</TableHead>
+//                             <TableHead >Template Id</TableHead>
+
+
+
+//                             <TableHead >Priority</TableHead>
+//                             <TableHead >Status</TableHead>
+
+//                             <TableHead >Action</TableHead>
+//                         </TableRow>
+//                     </TableHeader>
+//                     <tbody>
+//                         {audits.length === 0 ? <EmptyTable /> :
+//                             audits.map((audit) => {
+//                                 return (
+//                                     <TableRow key={audit.id}>
+//                                         <TableCell>{audit.id}</TableCell>
+//                                         <TableCell>{audit.client}</TableCell>
+//                                         <TableCell>{audit.template}</TableCell>
+
+
+//                                         <TableCell><PriorityBadge priority={audit.priority} /></TableCell>
+//                                         <TableCell><StatusBadge status={audit.status} /></TableCell>
+
+
+
+//                                         <TableCell>
+//                                             <div className="flex gap-2">
+
+//                                                 <Button icon="Edit" variant="Edit" iconSize="Small" onClick={() => {
+//                                                     setSelectedAudit(audit);
+//                                                     setShowModal(true);
+//                                                 }}>Edit</Button>
+
+//                                                 <Button icon="Details" iconSize="Small" variant="Details" onClick={() => { navigate(`/audits/audit-details/${audit.id}`) }}>Details</Button>
+//                                                 <Button icon="Archive" iconSize="Small" variant="Archive/Deactivate" onClick={() => {
+//                                                     setShowDeleteModal(true)
+//                                                     setSelectedAudit(audit)
+//                                                 }} >Archive</Button>
+//                                             </div>
+//                                         </TableCell>
+//                                     </TableRow>
+//                                 );
+//                             })
+//                         }
+//                     </tbody>
+//                 </Table>
