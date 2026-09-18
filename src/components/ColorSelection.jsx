ae94fc6239efb90325b7382633d8dd506212fbed
rameenali19 <rameenali1910@gmail.com>
2026-09-18 09:23:10 +0500

    changed shadow value

--- src/components/ColorSelection.jsx
+++ src/components/ColorSelection.jsx
@@ -9,7 +9,7 @@ function ColorSelection({ selectedColor, setSelectedColor }) {
         pieces.map((color) => {
           return (
             <div key={color.text}
-              className={`border-2 w-35 border-[#ff8127] h-30 rounded-lg flex items-center justify-center flex-col font-inter font-bold text-[#ff8127] transition hover:cursor-pointer ${selectedColor === color.text ? "shadow-xl bg-orange-200" : ""} hover:scale-105 `}
+              className={`border-2 w-35 border-[#ff8127] h-30 rounded-lg flex items-center justify-center flex-col font-inter font-bold text-[#ff8127] transition hover:cursor-pointer ${selectedColor === color.text ? "shadow-md bg-orange-200" : ""} hover:scale-105 `}
               onClick={() => {
                 setSelectedColor(color.text)
               }}
