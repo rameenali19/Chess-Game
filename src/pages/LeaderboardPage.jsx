edc1c73a040a849a30495c6a8f5b4d20694f3ba8
rameenali19 <rameenali1910@gmail.com>
2026-09-20 13:35:37 +0500

    adding imports

--- src/pages/LeaderboardPage.jsx
+++ src/pages/LeaderboardPage.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import LeaderboardHeader from "../components/LeaderboardHeader";
 import ApiChess from "../api/apiChess";
 import MyStats from "../components/MyStats";
