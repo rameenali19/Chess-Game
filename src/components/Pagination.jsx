e882d94e0cd9cea67b6a66aa9e45e89875a36d4e
rameenali19 <rameenali1910@gmail.com>
2026-09-20 13:29:50 +0500

    changing pagination a bit

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,31 +1,45 @@
 import Button from "./Button";
+
 function Pagination({ total, page, setPage }) {
-  const totalPages = Math.ceil(total / 10)
+  const totalPages = Math.ceil(total / 10);
+
   return (
-    <div className="flex justify-between px-2 mb-3">
-      {page < totalPages && (
-        <Button
-          text="Next Page"
-          variant="outline"
-          textSize="medium"
-          fontWeight="normal"
-          onClick={() => {
-            setPage(page + 1)
-          }}
-        />
-      )}
-      {page > 1 && (
+    <div className="flex items-center justify-between px-2 mb-3">
+
+      {page > 1 ? (
         <Button
           text="Previous Page"
           variant="outline"
           textSize="medium"
           fontWeight="normal"
           onClick={() => {
-            setPage(page - 1)
+            setPage(page - 1);
           }}
         />
+      ) : (
+        <div></div>
       )}
+
+      <span className="font-inter text-sm">
+        Page {page} of {totalPages}
+      </span>
+
+      {page < totalPages ? (
+        <Button
+          text="Next Page"
+          variant="outline"
+          textSize="medium"
+          fontWeight="normal"
+          onClick={() => {
+            setPage(page + 1);
+          }}
+        />
+      ) : (
+        <div></div>
+      )}
+
     </div>
-  )
+  );
 }
+
 export default Pagination;
